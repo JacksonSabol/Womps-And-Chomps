4fdc1f26e01692749d7f33e2fc6ee89884b3f5c5
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-04-21 17:41:51 -0700

    add responsiveness to navbar

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import './index.css';
 import { HomeBtn, NavbarItem } from '../Button';
 
-function Navbar(props) {
-  return (
-    <nav className="navbar">
-      <HomeBtn onClick={() => props.handlePageChange("Home")}>
-        {/* <img src={logo} className="navbar-brand-logo" alt="^_^" /> */}
-        Logo
-      </HomeBtn>
-      <div className="navbar-nav">
-        <NavbarItem
-          onClick={() => props.handlePageChange("Home")}
-        >
-          Home
+class Navbar extends Component {
+  // Set the initial state value for responsive hamburger menu
+  state = {
+    toggled: false
+  };
+
+  toggleMenu = () => {
+    const reverse = (this.state.toggled) ? false : true;
+    this.setState({
+      toggled: reverse
+    });
+  }
+
+  render() {
+    const iconToggled = this.state.toggled ? "toggled" : "";
+    const navbarToggled = this.state.toggled ? "open" : "";
+    return (
+      <nav className="navbar">
+        <p className="welcome-message">Welcome, {this.props.username}!</p>
+        <div className="navbar-hamburger">
+          <div className={`navbar-toggle ${iconToggled}`}>
+            <button type="button" className="navbar-toggler" onClick={() => this.toggleMenu()}>
+              <span className="navbar-toggler-bar bar1"></span>
+              <span className="navbar-toggler-bar bar2"></span>
+              <span className="navbar-toggler-bar bar3"></span>
+            </button>
+          </div>
+        </div>
+        <div className={`navbar-nav ${navbarToggled}`}>
+          <HomeBtn onClick={() => this.props.handlePageChange("Home")}>
+            {/* <img src={logo} className="navbar-brand-logo" alt="^_^" /> */}
+            Logo
+          </HomeBtn>
+          <NavbarItem
+            onClick={() => this.props.handlePageChange("Home")}
+          >
+            Home
           </NavbarItem>
-        <NavbarItem
-          onClick={() => props.handlePageChange("Events")}
-        >
-          Events
+          <NavbarItem
+            onClick={() => this.props.handlePageChange("Events")}
+          >
+            Events
           </NavbarItem>
-        <NavbarItem
-          onClick={() => props.handleLogout()}
-        >
-          Logout
+          <NavbarItem
+            onClick={() => this.props.handleLogout()}
+          >
+            Logout
           </NavbarItem>
-        <p className="welcome-message">Welcome, {props.username}!</p>
-      </div>
-    </nav>
-  );
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
